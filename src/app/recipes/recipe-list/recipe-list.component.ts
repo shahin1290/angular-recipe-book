3dc296567bd1fdde1ae77f0395a2503993a63ea0
shahin1290 <s.patowary@yahoo.com>
2018-12-17 00:14:23 +0100

    Adds model for recipe object, updates template to show the list of recipres

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "This is just for the test", 'https://torange.biz/photo/35/IMAGE/pumpkin-autumn-still-life-illustration-recipes-35541.jpg'),
+    new Recipe("A new recipe", "This is another for the test", 'https://images.pexels.com/photos/569356/pexels-photo-569356.jpeg?cs=srgb&dl=bangladesh-bangladesh-food-recipes-bd-ranna-569356.jpg&fm=jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
