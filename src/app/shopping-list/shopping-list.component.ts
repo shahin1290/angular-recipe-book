87fb4012cd3a935a295f4ce0195bb45c74b93076
shahin1290 <s.patowary@yahoo.com>
2018-12-17 10:47:44 +0100

    Creates a Ingredient model, instantiate Ingredient in shopping list and display in template

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingedient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] =  [
+    new Ingredient('Apples', 5),
+    new Ingredient('Banana', 4)
+  ];
   constructor() { }
 
   ngOnInit() {
