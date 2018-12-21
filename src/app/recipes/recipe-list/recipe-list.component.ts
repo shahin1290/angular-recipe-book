cde7ed9f36c0ba143a233d17afd49b8e2bb0ae92
shahin1290 <s.patowary@yahoo.com>
2018-12-21 11:14:52 +0100

    Moves code block from recipe list to item, adds custom property binding to show list of recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe("A test recipe", "This is just for the test", 'https://torange.biz/photo/35/IMAGE/pumpkin-autumn-still-life-illustration-recipes-35541.jpg'),
     new Recipe("A new recipe", "This is another for the test", 'https://images.pexels.com/photos/569356/pexels-photo-569356.jpeg?cs=srgb&dl=bangladesh-bangladesh-food-recipes-bd-ranna-569356.jpg&fm=jpg')
   ]
+
   constructor() { }
 
   ngOnInit() {
