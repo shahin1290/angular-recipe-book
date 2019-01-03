fec1d63ac6dbebf7a5af740c846deb0c19f82938
shahin1290 <s.patowary@yahoo.com>
2019-01-03 12:09:52 +0100

    Adds editing routes, retrieving route parameters, programmatic navigation to the edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +14,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -29,6 +30,10 @@ export class RecipeDetailComponent implements OnInit {
 
   addToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], { relativeTo: this.route})
   }
 
 }
